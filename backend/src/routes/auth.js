9d26465c120a2d22cc85c6084b39ad3f3ec61b56
henok <hhgebreslassie@miu.edu>
2022-03-15 11:47:44 -0500

    added a router for auth part of the app

---
+++ backend/src/routes/auth.js
@@ -0,0 +1,37 @@
+const { Router } = require("express")
+const { signIn, signUp, checkEmailExist } = require("../controllers/auth_controller")
+
+const route = Router();
+
+module.exports = authRouter = app => {
+    app.use('/auth', route)
+
+    route.post('/signin', async (req, res, next) => {
+        try {
+            const { user, accessToken } = await signIn(req.body)
+            res.status(200).json({ user, accessToken })
+        } catch (err) {
+            next(err)
+        }
+    })
+
+    route.post('/signup', async (req, res, next) => {
+        try {
+            const { newUser, accessToken } = await signUp(req.body)
+            res.status(200).json({ user: newUser, accessToken: accessToken })
+        } catch (err) {
+            next(err)
+        }
+    })
+    route.get('/checkEmailExists', async (req, res, next) => {
+        try {
+            const { email } = req.query;
+            const emailUsed = await checkEmailExist(email)
+            res.status(200).json({ success: true, data: { emailExists: emailUsed } })
+        } catch (err) {
+            next(err)
+        }
+    })
+
+}
+
